90304e53088e7f41886045b095cad4c4ddd0ef50
sindhu675 <sindhuk9070.sse@saveetha.com>
2026-03-24 16:15:46 +0530

    Add files via upload

---
+++ experiment 3.py
@@ -0,0 +1,13 @@
+# Water Jug Problem
+
+x = 4
+y = 3
+
+print("Fill 4L Jug")
+print("Pour water from 4L Jug to 3L Jug")
+print("Empty 3L Jug")
+print("Pour remaining water to 3L Jug")
+print("Fill 4L Jug again")
+print("Pour water to 3L Jug until it is full")
+
+print("Now 4L Jug contains 2 Litres of water")
